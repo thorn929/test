4e7fc171eeb317ed3f2b30eb2f1a9d7405f5e815
zcy <286727162@qq.com>
2020-01-06 13:27:05 +0800

    添加js压缩

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,30 @@
-// module.exports = {
-//   css: {
-//     loaderOptions: {
-//       less: {
-//         javascriptEnabled: true
-//       }
-//     }
-//   }
-// }
+
+const Terser = require('terser-webpack-plugin')
+
+module.exports = {
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  publicPath: '/',
+  runtimeCompiler: true,
+  productionSourceMap: false,
+  configureWebpack: {
+    optimization: {
+      minimizer: [
+        new Terser({
+          test: /\.m?js$/
+        })
+      ],
+      splitChunks: {
+        cacheGroups: {
+          vue: {
+            name: 'vue',
+            test: /[\\/]node_modules[\\/](vue|vue-router|vuex)[\\/]/,
+            chunks: 'all',
+            priority: 1,
+            reuseExistingChunk: true,
+            enforce: true
+          }
+        }
+      }
+    }
+  }
+}
